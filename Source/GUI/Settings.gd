548bddc887c341f6045042807a45d2074262fb6f
Adrian Bedregal <rolandobedregal321@gmail.com>
2023-10-08 13:39:23 -0500

    Added back behaviour on save settings

--- Source/GUI/Settings.gd
+++ Source/GUI/Settings.gd
@@ -18,6 +18,7 @@ func _on_render_distance_slider_value_changed(value: float) -> void:
 
 func _on_save_button_down() -> void:
 	GeneratorConfig.chunk_render_distance = render_distance_slider.value;
+	emit_signal("back_button_down");
 
 
 func _on_back_button_down() -> void:
